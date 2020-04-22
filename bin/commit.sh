81ee8d210c35cd21012a1b7b4dd953aa80e5cada
wangxuemin@zzlx.org <wangxuemin@zzlx.org>
2020-04-22 18:20:39 +0800

    20200422_自动化提交

--- bin/commit.sh
+++ bin/commit.sh
@@ -7,11 +7,16 @@ APP_WORK_TREE=$(dirname $(cd $(dirname $0); pwd))
 # -C 指定工作目录 
 
 _commit_and_push() {
+
   if [ -d "$APP_WORK_TREE/.git" ];then
     git -C $APP_WORK_TREE add -A .
-    git -C $APP_WORK_TREE commit -m "自动提交_$(date "+%Y%m%d")"
+    git -C $APP_WORK_TREE commit -m "$(date "+%Y%m%d")_自动化提交"
+  else
+    git -C $APP_WORK_TREE init
     return
   fi
+
+  echo "自动化提交完成"
 }
 
 _commit_and_push
